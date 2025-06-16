dade28924d82b0e9df64bf1b4ff1652743984655
Paul Douane <paul.douane@gmail.com>
2025-06-16 22:15:49 +0000

    get args / parse options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
+struct Options {}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut args_iter = args.iter();
+    let mut options: Option<Options> = None;
+
+    // Skip the target arg
+    args_iter.next();
+
+    if let Some(arg) = args_iter.next() {
+        let ch = arg.chars().next().unwrap();
+
+        if ch == '-' {
+            options = Some(Options { });
+        } else { println!("{arg}"); }
+    }
+
+    if let Some(options) = options {
+        println!("Option found");
+    }
 }
